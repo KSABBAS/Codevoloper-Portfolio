c6ef891fc697da0a5cacf3b8424d7d899cd47d59
mohmmed <85923186+mohmmed50@users.noreply.github.com>
2024-12-16 00:11:26 +0200

    initializeApp

--- codeveloper_portfolio/lib/main.dart
+++ codeveloper_portfolio/lib/main.dart
@@ -4,9 +4,14 @@ import 'package:codeveloper_portfolio/Data/membersData.dart';
 import 'package:codeveloper_portfolio/View/Home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // put you member data function here
   MemberClass.setMembers([
     [
--- codeveloper_portfolio/macos/Flutter/GeneratedPluginRegistrant.swift
+++ codeveloper_portfolio/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
 import package_info_plus
 import url_launcher_macos
 import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
